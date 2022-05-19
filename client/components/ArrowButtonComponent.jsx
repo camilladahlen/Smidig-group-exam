b9bdec4c4bc73225eb7220f209c4f5f82ef7e706
Herman Svendsen Loen <71780070+HermanLoen@users.noreply.github.com>
2022-05-19 13:24:15 +0200

    Feature/arrow button component (#16)
    * ArrowBtn looks good
    
    * fixed scaling

--- client/components/ArrowButtonComponent.jsx
+++ client/components/ArrowButtonComponent.jsx
@@ -1,8 +1,9 @@
 
-export function ArrowButton() {
+export function ArrowButton({value}) {
     return(
         <div className={"arrowButton-container"}>
-                <input type={"Button"} value={"Button"} className={"button-container"}/>
+                <input type={"Button"} value={value} className={"button-container"}/>
+
             <span className={"arrow-right"}>
                 <i className="fa-solid fa-arrow-right"></i>
             </span>
