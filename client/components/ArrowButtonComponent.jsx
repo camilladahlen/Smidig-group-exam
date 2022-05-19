d79c69040b571bae6b36369ec2a0148467b2594a
Herman Svendsen Loen <71780070+HermanLoen@users.noreply.github.com>
2022-05-19 12:37:49 +0200

    ArrowBtn looks good (#15)

---
+++ client/components/ArrowButtonComponent.jsx
@@ -0,0 +1,11 @@
+
+export function ArrowButton() {
+    return(
+        <div className={"arrowButton-container"}>
+                <input type={"Button"} value={"Button"} className={"button-container"}/>
+            <span className={"arrow-right"}>
+                <i className="fa-solid fa-arrow-right"></i>
+            </span>
+        </div>
+    )
+}
