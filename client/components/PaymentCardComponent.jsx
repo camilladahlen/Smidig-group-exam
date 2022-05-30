6758e4de37639622586f6dc1241d7a2285d27475
camilladahlen <camidahlen@gmail.com>
2022-05-30 15:12:05 +0200

    added some padding

--- client/components/PaymentCardComponent.jsx
+++ client/components/PaymentCardComponent.jsx
@@ -7,11 +7,11 @@ export function PaymentCard({ title, revenue, perks }) {
     <div className={"card-content card is-flex is-flex-direction-column"}>
       <div className="media-content">
         <p className="title is-4">{title}</p>
-        <p className="subtitle is-6 mb-0 pb-1">{revenue}</p>
+        <p className="subtitle is-6 mb-0 pb-2">{revenue}</p>
         <ul>
           {perks &&
             perks.map((p) => (
-              <li>
+              <li className={"pb-1"}>
                 <span className="icon is-small">
                   <i className="fas fa-check" />
                 </span>
